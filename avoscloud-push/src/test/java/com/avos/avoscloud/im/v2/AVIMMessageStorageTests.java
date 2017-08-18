9b925b35270b60c4bcf170587a4a910cf58b5df7
jwfing <jwfing@gmail.com>
2017-08-18 16:44:43 +0800

    fiel rename: TestConfig -> PushTestConfig

---
+++ avoscloud-push/src/test/java/com/avos/avoscloud/im/v2/AVIMMessageStorageTests.java
@@ -0,0 +1,20 @@
+package com.avos.avoscloud.im.v2;
+
+import com.avos.avoscloud.BuildConfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by fengjunwen on 2017/8/18.
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, manifest = Config.NONE, sdk = 21)
+public class AVIMMessageStorageTests {
+  @Test
+  public void testDummy() {
+    ;
+  }
+}
